ee3f708b4f283d6d084e28e2eb7d28e656160eb5
saikiran-private <141546713+saikiran-private@users.noreply.github.com>
2025-01-26 12:05:13 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "vpc-cidr-block" {
+    description = "This is the CIDR of the VPC"
+}
+
+variable "subnet-cidr" {
+    description = "This is the subnet cidr"
+  
+}
+
+variable "az" {
+    description = "this is the availability zone"
+  
+}
